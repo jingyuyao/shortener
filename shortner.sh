a3bd2e4068f6da5067d32a7d51a916f781d47cd0
Jingyu Yao <yjy.emails@gmail.com>
2016-09-24 14:24:06 -0400

    Fixed a refactoring typo

--- shortner.sh
+++ shortner.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Runs the generate jar with config.yml
-# e.g. ./shortner.sh server
+# Runs the generate jar
+# e.g. ./shortner.sh server config.yml
 version=$(head -n 1 target/classes/version.txt)
 java -jar target/shortner-${version}.jar "$@"
